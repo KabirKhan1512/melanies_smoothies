c819d9dcb8ae76fc58911506468231747e3bbb7e
Muhammad Kabir ud din <kabirkhan1512@gmail.com>
2025-01-30 11:35:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
 # New section to display smoothie nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
+st.text(smoothiefroot_response.json())
 
 
 
